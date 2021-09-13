caa22ca3c901626b48cd5cf8aaa0a6b69c3b9dae
NamrathaNShetty <namratanshetty@gmail.com>
2021-09-13 21:22:11 +0530

    [Namratha] Add: Add part time and calculate wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -13,20 +13,26 @@ def checkAttendance():
     """
     Description:
         This function is to check whether employee is present or absent.
-        Employee daily wage is calculated for full day.
+        Employee daily wage is calculated for full day and part time.
     """    
+
+    isFullTime = 1
+    isPartTime = 2
+    day_Hour=0
 
     wage_Per_Hour = 20
     
-    emp_Check = random.randint(0,1)
+    emp_Check = random.randint(0,2)
 
-    if emp_Check == 0:
-        print("Employee is present")
-        full_day_Hour = 8
+    if emp_Check == isFullTime:
+        day_Hour = 8
+    elif emp_Check == isPartTime:
+        day_Hour = 4 
     else:
         print("Employee is absent")
+        
 
-    employee_Wage = wage_Per_Hour * full_day_Hour
+    employee_Wage = wage_Per_Hour * day_Hour
     print("Employee wage is:", employee_Wage)
 
 if __name__ == "__main__":
