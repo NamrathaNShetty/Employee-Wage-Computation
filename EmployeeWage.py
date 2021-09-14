024e151ee666840803be62efef526e7cf12f9a65
NamrathaNShetty <namratanshetty@gmail.com>
2021-09-14 09:38:46 +0530

    [Namratha] Add: Store the day daily wage with total wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,15 +1,15 @@
 """
 @Author: Namratha N Shetty
-@Date: 2021-09-13 16:36:00 
+@Date: 2021-09-13 23:36:00 
 @Last Modified by: Namratha N Shetty
-@Last Modified time: 2021-09-13 16:37:00
+@Last Modified time: 2021-09-14 1:37:00
 @Title : To check employee is present or absent
 """
 
 import random
 print("Welcome to employee wage computation")
 
-def calculateHour(attendanceStatus):
+def calculateHour(attendance_Status):
     """
     Description:
         This function determine the work hours
@@ -18,20 +18,20 @@ def calculateHour(attendanceStatus):
     Return:
         the functution return 8 or 4 or 0 value as work hour
     """
-    if attendanceStatus == isFullTime:
+    if attendance_Status == isFullTime:
         day_Hour = 8
-    elif attendanceStatus == isPartTime:
+    elif attendance_Status == isPartTime:
         day_Hour = 4 
     else:
         day_Hour = 0
     return day_Hour
-    
-def getWorkHours(totalWorkingHours):
-    '''
+
+def getWorkHours(total_Working_Hours):
+    """
     Description:
         this function calculate working hours
-    '''
-    print("Total working hours:", totalWorkingHours)    
+    """
+    print("Total working hours:", total_Working_Hours)     
 
 def calculateWage():
     """
@@ -40,27 +40,29 @@ def calculateWage():
     Return:
         this function return total employee wage of a month
     """
+    daily_Wage_List = []
     wage_Per_Hour = 20
     working_Days = 0
     total_Working_Hours = 0
     maximum_Working_Days = 20
     maximum_Working_Hours = 100
     total_Wage = 0
-    daily_Wage_List = []
+    wage_Dict = {}
 
     while working_Days < maximum_Working_Days and total_Working_Hours < maximum_Working_Hours:
         attendance = random.randint(0,2)
-        attendanceStatus = switcher.get(attendance)
-        working_Hours = calculateHour(attendanceStatus)
+        attendance_Status = switcher.get(attendance)
+        working_Hours = calculateHour(attendance_Status)
         daily_Wage = working_Hours * wage_Per_Hour
         daily_Wage_List.append(daily_Wage)
         total_Wage += daily_Wage
         total_Working_Hours += working_Hours
         working_Days += 1
-    getWorkHours(total_Working_Hours)
-    print("Daily wage:", daily_Wage_List)
-    return total_Wage
-
+        wage_Dict[working_Days] = {
+            "Daily Wage:" : daily_Wage,
+            "Total Wage:" : total_Wage
+        }
+    return wage_Dict
 
 absent = 0
 isFullTime = 1
@@ -69,7 +71,7 @@ isPartTime = 2
 switcher = {
     0: absent,
     1: isFullTime,
-    2: isFullTime,
+    2: isPartTime,
 }
 
-print("Total employee wage for month is:",calculateWage())
+print("Total employee wage for month is:",calculateWage())    
